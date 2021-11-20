5618dfc2e7d0aa7c49b25498ed4291af3e8c7bde
Inna99 <ikunygina@yandex.ru>
2021-11-20 13:00:35 +0300

    fix model

--- date_app/models.py
+++ date_app/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 
 class User(models.Model):
-    # img = models.ImageField(upload_to='article', height_field=100, width_field=100)
-    gender = models.CharField(max_length=20)
+    GENDER_CHOICES = [
+        ('F', 'female'),
+        ('M', 'male'),
+    ]
+    img = models.ImageField(upload_to='article', height_field=100, width_field=100)
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    # email = models.EmailField()
+    email = models.EmailField()
     date_created = models.DateTimeField(auto_created=True)
 
-    # def __str___(self):
-    #     return f"{self.first_name} {self.last_name}"
+    def __str___(self):
+        return f"{self.first_name} {self.last_name}"
--- date_app/serializers.py
+++ date_app/serializers.py
@@ -6,4 +6,5 @@ from date_app.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["gender", "email", "first_name"]
+        # fields = ["gender", "last_name", "first_name"]
+        fields = '__all__'
